dd84bb11ca7334181354ff8fb3470caa0fefe6d1
link-bands <ireallyagreewithyou@gmail.com>
2024-03-10 15:31:51 +0800

    test

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <Greet />
+</template>
+
+<style scoped>
+.logo.vite:hover {
+  filter: drop-shadow(0 0 2em #747bff);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #249b73);
+}
+</style>
---
+++ src/components/Greet.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from "vue";
+// When using the Tauri API npm package:
+import { invoke } from "@tauri-apps/api/tauri";
+
+onMounted(() => {
+  // console.log(a*b);
+});
+
+const myCustomCommand = () => {
+  const a = 0.07;
+  const b = 100;
+  invoke('my_custom_command', { a, b }).then((message) => console.log(message));
+};
+</script>
+
+<template>
+  <button @click="myCustomCommand">触发rust方法</button>
+</template>
